fbf2b4e88436f301a34c7ba88287855f06172dc0
Seb James <seb@logmykidsoff.com>
2017-05-23 13:59:14 +0100

    Sacc vs luminance - commit to try running on ace2

--- batch_scripts/expt_sacc_vs_luminance/plot_sacc_vs_l.m
+++ batch_scripts/expt_sacc_vs_luminance/plot_sacc_vs_l.m
@@ -14,26 +14,31 @@ plottraj = 0;
 
 colours = {'r','b','g','k','c','m','r--','b--','g--','k--','c--','m--'};
 colcount = 1;
-for i = [1 : 0.1 : 1.3]
+for i = [0.5 : 0.2 : 2.1]
     resname = ['r_' num2str(thetax) '_' num2str(thetay) '_' num2str(i) '.dat'];
     resdatname = ['r_' num2str(thetax) '_' num2str(thetay) '_' num2str(i)];
     resdatname = strrep (resdatname, '.', 'p');
     resdatname = strrep (resdatname, '-', 'm');
 
-    load (['results/' resname]); % loads struct variable called result
+    rnm = ['results/' resname];
+    display(['Loading file ' rnm])
+    load (rnm); % loads struct variable called result
+
     r = struct_merge (r, result);
 
-    rr = [rr; result.(resdatname)];
+    if (size(result.(resdatname))(2) == 9)
+        rr = [rr; result.(resdatname)];
+    end
 
     % Also plot each trajectory from the model run data
     if plottraj
-    for j=1:num_runs
-        filepath=['/fastdata/' getenv('USER') '/oculomotorRX0RY' num2str(thetay) '_' num2str(i) '_' num2str(j)];
-        A=load_ocm_min(filepath);
-        figure(33);
-        hold on;
-        plot (A.eyeRy, colours{colcount})
-    end
+        for j=1:num_runs
+            filepath=['/fastdata/' getenv('USER') '/oculomotorRX0RY' num2str(thetay) '_' num2str(i) '_' num2str(j)];
+            A=load_ocm_min(filepath);
+            figure(33);
+            hold on;
+            plot (A.eyeRy, colours{colcount})
+        end
     end
     colcount = colcount + 1;
 
