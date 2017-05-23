062fb8e41dcb9e6b9f66506bbbad9cdb7964caae
Seb James <seb@logmykidsoff.com>
2017-05-23 11:55:10 +0100

    Choose the time of the first activity in any axis.

--- batch_scripts/include/find_peaktime.m
+++ batch_scripts/include/find_peaktime.m
@@ -6,6 +6,7 @@
 function peaktime = find_peaktime (basepath)
     peaktime = -1;
     [llbn_left, count] = load_sc_data ([basepath '/log/LLBN_left_a_log.bin'], 1);
+    display(['Loaded ' num2str(count) ' data points from llbn_l']);
 
     % We expect the run to have 450 datapoints - 0.45 s runtime
     % with 1 ms per step.
@@ -14,15 +15,20 @@ function peaktime = find_peaktime (basepath)
         return
     end
 
-    if isempty(peaktime = min (find (llbn_left>0)))
-        [llbn_right, count] = load_sc_data ([basepath '/log/LLBN_right_a_log.bin'], 1);
-        if isempty(peaktime = min (find (llbn_right>0)))
-            [llbn_up, count] = load_sc_data ([basepath '/log/LLBN_up_a_log.bin'], 1);
-            if isempty(peaktime = min (find (llbn_up>0)))
-                [llbn_down, count] = load_sc_data ([basepath '/log/LLBN_down_a_log.bin'], 1);
-                peaktime = min (find (llbn_down>0));
-            end
-        end
-    end
+    % Load the rest
+    [llbn_right, count] = load_sc_data ([basepath '/log/LLBN_right_a_log.bin'], 1);
+    [llbn_up, count] = load_sc_data ([basepath '/log/LLBN_up_a_log.bin'], 1);
+    [llbn_down, count] = load_sc_data ([basepath '/log/LLBN_down_a_log.bin'], 1);
+
+    peaktime_l = min (find (llbn_left>0));
+    peaktime_r = min (find (llbn_right>0));
+    peaktime_u = min (find (llbn_up>0));
+    peaktime_d = min (find (llbn_down>0));
+    % If any of the above are empty, then there will be
+    % correspondingly fewer entries in peaktimes:
+    peaktimes = [peaktime_l peaktime_r peaktime_u peaktime_d];
+    % So min(peaktimes) should be safe:
+    peaktime = min(peaktimes);
+
     display (['peaktime = ' num2str(peaktime)]);
 end
