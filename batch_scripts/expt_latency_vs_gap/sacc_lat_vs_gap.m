4ffdc8adbd05044a9fdbc8b3221d5d430b747bb1
Seb James <seb@logmykidsoff.com>
2017-05-25 14:05:30 +0100

    Code to run expt_latency_vs_gap in same way as sacc_vs_targetpos and sacc_vs_luminance

---
+++ batch_scripts/expt_latency_vs_gap/sacc_lat_vs_gap.m
@@ -0,0 +1,85 @@
+%% Run the model to find the saccade latency and end position for a
+%% given target location, gap and target luminance. Fixation
+%% luminance is hardcoded in this function.
+%%
+%% Results are saved in ./results/
+%%
+%% Usage:
+%%
+%%   sacc_lat_vs_gap (targetThetaX, targetThetaY, num_par_runs,
+%%   gap_ms, preflight_options='')
+%%
+function sacc_lat_vs_gap (targetThetaX, targetThetaY, num_par_runs, gap_ms, lum, preflight_options='')
+
+    page_screen_output(0);
+    page_output_immediately(1);
+
+    if ~(floor(targetThetaX) == targetThetaX) || ~(floor(targetThetaY) == targetThetaY)
+        display ('Integers only for targetThetaX/targetThetaY')
+        return;
+    end
+
+    % Configure params object for run_simulation_multi
+    params.cleanup = 0;
+    params.num_runs = num_par_runs;
+
+    if ~isempty(preflight_options)
+        params.preflight_options = preflight_options;
+    end
+
+    [d, msg, msgid] = mkdir (['/fastdata/' getenv('USER')])
+    [d, msg, msgid] = mkdir (['/fastdata/' getenv('USER') '/OMM_NeuroMuscular'])
+    model_dir = ['/fastdata/' getenv('USER') '/OMM_NeuroMuscular/SVG' num2str(targetThetaX) '_' num2str(targetThetaY) '_' num2str(gap_ms)]
+
+    model_tag = getenv('OMMODEL');
+    if isempty(model_tag)
+        display('Please set the environment variable OMMODEL to something like "Model3"')
+        return
+    end
+    origin_model_dir = [getenv('HOME') '/OMM_NeuroMuscular/' model_tag];
+
+    % Copy origin_model_dir into model_dir
+    copyfile (origin_model_dir, model_dir);
+
+    % This writes into the origin model directory. When running
+    % many in parallel, these get mixed up. So make a copy of the
+    % model.
+    params.targetCross=0; % 1 if target should be the cross shape.
+    params.targetLuminance=lum;
+    params.targetWidthX=4;
+    params.targetWidthY=4;
+    params.fixCross=0;
+    params.fixLuminance=0.5;
+    params.fixWidthX=2;
+    params.fixWidthY=2;
+    params.fixOn=0;
+    params.targetOff=1;
+    params.cleanup=1; % Set to 1 to clean up used files.
+
+    % Determined by the gap argument:
+    params.targetOn = 0.2;
+    params.fixOff = 0.2-gap_ms;
+    % Also from args:
+    params.targetThetaX=targetThetaX;
+    params.targetThetaY=targetThetaY;
+
+    write_single_luminance_with_fix ([model_dir '/luminances.json'], params);
+
+    output_dirs = setup_model_directories ([targetThetaX, targetThetaY], gap_ms);
+    [ eyeRyAvg, eyeRySD, eyeRyFinals, peakPos, startMove ] = run_simulation_multi (model_dir, output_dirs, params)
+
+    resname = ['r_' num2str(targetThetaX) '_' num2str(targetThetaY) '_G' num2str(gap_ms) '_L' num2str(lum) '.dat'];
+    resdatname = ['r_' num2str(targetThetaX) '_' num2str(targetThetaY) '_G' num2str(gap_ms) '_L' num2str(lum)];
+    resdatname = strrep (resdatname, '.', 'p');
+    % You can't put minus signs in the resdat name, either.
+    resdatname = strrep (resdatname, '-', 'm');
+
+    vs = [targetThetaX, targetThetaY, gap_ms, lum, eyeRyAvg, eyeRySD, mean(startMove)-(1000.*params.targetOn), std(startMove)];
+
+    result = struct();
+    result.params = params;
+    result.(resdatname) = vs;
+
+    save (['results/' resname], 'result');
+
+end
---
+++ batch_scripts/expt_latency_vs_gap/sacc_lat_vs_gap.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Run through a set of experiments with different gap/overlaps,
+# qsubbing NUM_RUNS of the model for each gap. Results (average
+# saccade size, saccade size SD, latency, latency SD) are stored in
+# octave data files. Load these and plot using plot_sacc_vs_g.m
+
+GAPSTART=-10 # Will need a longer expt to go more than 80 ms overlap
+GAPINC=2
+GAPEND=10
+
+THETAX=0
+THETAY=-8
+NUM_RUNS=12
+
+LUMVAL=0.7
+
+mkdir -p results
+
+P_STR='iceberg'
+if [ -d /usr/local/abrg ]; then
+    P_STR='ace2'
+fi
+
+# Use an env. variable to select which model to run.
+export OMMODEL='Model3'
+
+for gapval in `seq -80 20 80` `seq -10 2 10` 1 -1; do
+
+    if [ ${P_STR} = 'iceberg' ]; then
+        # 1) write out a script we can qsub for the luminance:
+        cat > script${gapval}.sh <<EOF
+#!/bin/bash
+#$ -l mem=4G
+#$ -l rmem=4G
+#$ -l h_rt=4:59:00
+#$ -m ae
+#$ -M seb.james@sheffield.ac.uk
+
+EOF
+    elif [ ${P_STR} = 'ace2' ]; then
+        cat > script${gapval}.sh <<EOF
+#!/bin/bash
+
+EOF
+    fi
+
+    cat >> script${gapval}.sh <<EOF
+pushd ${HOME}/OMM_NeuroMuscular/batch_scripts/expt_latency_vs_gap
+octave -q --eval "octave_run_test"
+oct_run_rtn=\$?
+if [ \$oct_run_rtn -gt "0" ]; then
+    echo "Failed to run Octave on this host, exiting."
+    popd
+    exit 1
+fi
+popd
+
+octave -q --eval "sacc_lat_vs_gap(${THETAX},${THETAY},${NUM_RUNS},${gapval},${LUMVAL})"
+exit 0
+EOF
+
+    # 2) and then qsub it:
+    PROJECT_TAG=''
+    if [ ${P_STR} = 'iceberg' ]; then
+        PROJECT_TAG='-P insigneo-notremor'
+    fi
+    qsub ${PROJECT_TAG} -N SVG${gapval} -wd ${HOME}/OMM_NeuroMuscular/batch_scripts/expt_latency_vs_gap -o results/SVG${gapval}.out -j y ./script${gapval}.sh
+
+    # 3) Clean up the script
+    rm -f ./script${gapval}.sh
+
+done
