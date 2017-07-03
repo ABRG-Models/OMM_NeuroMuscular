700cff9f47716ea80507ceca19ae5a46f98ee916
Seb James <seb.james@sheffield.ac.uk>
2017-07-03 17:36:25 +0100

    Implements a widening Gaussian with a shift in r to avoid the big drop-off in weights.

--- lab_book/widening_gaussian.m
+++ lab_book/widening_gaussian.m
@@ -2,30 +2,34 @@
 
 W_nfs = 50
 % Widening Factor
-WF = 25
-sigma = 2
+WF = 15
+sigma = 3
 E2 = 2.5
 
-r = [1:50];
+rshift = 5;
+r = [1+rshift:50+rshift];
 
 M_f =  W_nfs ./ (E2 .* log ((r/(2.*E2))+1));
 figure(1)
 plot (r, M_f);
 xlabel('r');
-ylabel('Mag factor');
+ylabel('MagFactor(r)');
 
 _sigma = WF.*sigma./M_f;
-_SIGMA=repmat(_sigma, 50, 1);
+_SIGMA=repmat(_sigma, 50, 1)
 
 figure(2)
 plot (r, _sigma);
 xlabel('r');
-ylabel('_sigma');
+ylabel('_sigma(r)');
 
 %normterm = 1./(power(_sigma,2).*2.*pi);
 %NORMTERM = repmat(normterm, 50, 1);
 normterm = 1./(power(sigma,2).*2.*pi);
 NORMTERM = normterm;
+figure(4)
+plot (r, normterm);
+title ('normterm(r)');
 
 d = [1:50];
 D = repmat (d, 50, 1)';
@@ -36,4 +40,4 @@ surf(GAUSS)
 xlabel('r')
 ylabel('d')
 ylim([1,15])
-view([230 30])
+view([268 0.1])
