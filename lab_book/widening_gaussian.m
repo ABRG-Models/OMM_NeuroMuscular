b91de87bb6714dbd76a80a3f7d31a75987d6ddac
Seb James <seb.james@sheffield.ac.uk>
2017-07-04 15:29:08 +0100

    Latest widening gaussian maths

--- lab_book/widening_gaussian.m
+++ lab_book/widening_gaussian.m
@@ -1,44 +1,59 @@
 % octave file to investigate the widening gaussian projective field
+% This is then implemented in the model as the WideningGaussian
+% python connection script.
 
+% The neural field size - never changes in this model, so this is
+% not a parameter.
 W_nfs = 50
-% Widening Factor
-WF = 15
-sigma = 3
+
+%
+% The following will be parameters to the WideningGaussian script
+%
+% The sigma constant from which sigma(r) is computed
+sigma = 20
+% E2 participates in the magnification factor. In practice this is
+% not changed, but it's still included as a param.
 E2 = 2.5
+% An r shift - required in the Python script due to indexing from 0
+_rshift = 0;
+% A normalisation power
+normpower = 1.5;
 
-rshift = 0;
-r = [1+rshift:50+rshift];
+r = [1+_rshift:50+_rshift];
 
-M_f =  W_nfs ./ (E2 .* log ((r/(2.*E2))+1));
+
+M_f =  W_nfs ./ (E2 .* log (((r)/(2.*E2))+1));
+
+rshift = 5; % Past the foveal region
+M_f(1:rshift) = M_f(rshift);
+
 figure(1)
 plot (r, M_f);
 xlabel('r');
 ylabel('MagFactor(r)');
+title ('MagFactor');
 
-_sigma = WF.*sigma./M_f;
+_sigma = sigma./M_f;
 _SIGMA=repmat(_sigma, 50, 1);
 
 figure(2)
 plot (r, _sigma);
 xlabel('r');
-ylabel('_sigma(r)');
+ylabel('(underscore)sigma(r)');
+title('sigma(r)');
 
-%normterm = 1./(power(_sigma,2).*2.*pi);
-%NORMTERM = repmat(normterm, 50, 1);
+d = [1:50];
+D = repmat (d, 50, 1)';
 
-%normterm = 1./(power(sigma,2).*2.*pi);
-%NORMTERM = normterm;
-
-NORMTERM = 1./_sigma;
+% This gives good normalisation.
+normterm = 1./power(_sigma,normpower);
 
 figure(4)
 plot (r, normterm);
 title ('normterm(r)');
 
-d = [1:50];
-D = repmat (d, 50, 1)';
 
-GAUSS = NORMTERM.*exp(-0.5.*power(D./_SIGMA,2));
+GAUSS = normterm.*exp(-0.5.*power(D./_SIGMA,2));
 figure(3)
 surf(GAUSS)
 xlabel('r')
@@ -46,4 +61,8 @@ ylabel('d')
 ylim([1,15])
 view([268 0.1])
 
-sum(GAUSS, 1);
+
+figure(5);
+plot (r, sum(GAUSS, 1));
+title('Sum of GAUSS surface (figure 3) along d axis')
+ylim([0 1.25])
