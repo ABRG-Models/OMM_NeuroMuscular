ec2edd6f0fb6224b32d08b5812fa280cdbde63ae
Seb James <seb.james@sheffield.ac.uk>
2017-07-21 15:55:38 +0100

    Updates map_modelling2 to modify the sines so that we get correct right, down and ALSO oblique saccades

--- batch_scripts/weight_theoretical/map_modelling2.m
+++ batch_scripts/weight_theoretical/map_modelling2.m
@@ -10,7 +10,7 @@ y3=exp(0.2*(x-39));
 
 zlim_max=2.5;
 
-figure(10);
+figure(110);
 plot (x,y1);
 hold on;
 plot (x,y2,'r');
@@ -20,36 +20,126 @@ plot (x,y2,'r');
 Y = repmat(y3,50,1);
 
 % Do the sine part in higher precision first, to ensure symmetry
-% (rather than having circ commands of 12 and 13). Use 500 pixels
+% (rather than having circ commands of 12 and 13). Use 5000 pixels
 % rather than 50.
 %
-% Here's half a sine wave.
-sine_width = 1900;
+% Here's half a sine wave. Both at 1900 was my starting point.
+sine_width1 = 1900; % Vertical. eyeRx
+sine_width2 = 1900; % Horizontal. eyeRy
+
 zer=zeros(1,5000);
+
 x2=[0:4999];
-z = sin(x2.*pi./sine_width);
-mask = find(x2<sine_width);
-mask2 = find(x2>=sine_width);
-curv1 = [z(mask),zer(mask2)]';
-figure(5); clf;
-plot (x2,curv1);
-title ('single sine on 5000 wide space')
+z1 = sin(x2.*pi./sine_width1);
+z2 = sin(x2.*pi./sine_width2);
+
+mask1A = find(x2<sine_width1);
+mask1AL = find(x2<sine_width1/2);
+mask1AR = find(x2<sine_width1 & x2>=sine_width1/2);
+mask1B = find(x2>=sine_width1);
+
+mask2A = find(x2<sine_width2);
+mask2AL = find(x2<sine_width2/2);
+mask2AR = find(x2<sine_width2 & x2>=sine_width2/2);
+mask2B = find(x2>=sine_width2);
+
+maxpoint1 = sine_width1/2; % Peak of sine1
+crosspoint1L = maxpoint1 - 5000/8;
+crosspoint1R = maxpoint1 + 5000/8;
+maxpoint2 = sine_width1/2;
+crosspoint2L = maxpoint2- 5000/8;
+crosspoint2R = maxpoint2 + 5000/8;
+
+% Empirically determined suitable multiplier to give a -7,-7 saccade
+m_at_crosspoint1 = 0.65; % Vert
+m_at_crosspoint2 = 1.7;  % Horz
+
+
+%
+% ones as a multiplication function
+%
+onesmultfunc = ones(1,5000);
+
+%
+% Linear multiplier function
+%
+
+grad1L = (1-m_at_crosspoint1)./(maxpoint1 - crosspoint1L);
+offs1L = 1-(grad1L.*maxpoint1);
+grad1R = (1-m_at_crosspoint1)./(maxpoint1 - crosspoint1R);
+offs1R = 1-(grad1R.*maxpoint1);
+linmultfunc1L = (grad1L.*x2)+offs1L;
+linmultfunc1R = (grad1R.*x2)+offs1R;
+
+grad2L = (1-m_at_crosspoint2)./(maxpoint2 - crosspoint2L);
+offs2L = 1-(grad2L.*maxpoint2);
+grad2R = (1-m_at_crosspoint2)./(maxpoint2 - crosspoint2R);
+offs2R = 1-(grad2R.*maxpoint2);
+linmultfunc2L = (grad2L.*x2)+offs2L;
+linmultfunc2R = (grad2R.*x2)+offs2R;
+
+%
+% Sigmoidal multiplier functions
+%
+
+k = 0.006;
+sigoffs = 400;
+sigmultfunc1L = 0.3 + (1 ./ (1.428 + exp(-k.*(x2.-sigoffs))));
+% Normalise so that function is 1 at sine_width1/2:
+sigmultfunc1L = sigmultfunc1L ./(sigmultfunc1L(sine_width1/2));
+sigmultfunc1Latcross = 0.3 + (1 ./ (1.428 + exp(-k.*(crosspoint1L-sigoffs))))
+
+k2 = 0.006;
+sigoffs2 = 4700;
+sigmultfunc2L = fliplr(1 + (1 ./ (1 + exp(-k2.*(x2.-sigoffs2)))));
+sigmultfunc2L = sigmultfunc2L ./(sigmultfunc2L(sine_width2/2));
+sigmultfunc2Latcross = sigmultfunc2L(crosspoint2L)
+
+
+% Choose one of the above modifiers(ones, linear or sigmoidal):
+multfunc1L = sigmultfunc1L; % sigmultfunc1L, linmultfunc1L or onesmultfunc;
+multfunc2L = sigmultfunc2L; % sigmultfunc2L, linmultfunc2L or onesmultfunc;
+
+% Flipping same for all functions:
+multfunc1R = [fliplr(multfunc1L(mask1A)), zer(mask1B)];
+multfunc2R = [fliplr(multfunc2L(mask2A)), zer(mask2B)];
+
+z1L = z1.*multfunc1L;
+z1R = z1.*multfunc1R;
+
+z2L = z2.*multfunc2L;
+z2R = z2.*multfunc2R;
+
+curv1 = [z1L(mask1AL), z1R(mask1AR), zer(mask1B)]'; % Vertical. eyeRx
+curv2 = [z2L(mask2AL), z2R(mask2AR), zer(mask2B)]'; % Horz
+
+figure(105); clf;
+plot (x2,curv1,'r');
+hold on;
+plot (x2,curv2, 'g');
+plot (x2, multfunc1L, 'k')
+plot (x2, multfunc2L, 'm')
+
+title ('single sine(s) on 5000 wide space');
 % Now rotate and shift these sines.
 
-first_shift = -sine_width./2;
-shift = 5000./4;
+first_shift1 = -sine_width1./2;
+first_shift2 = -sine_width2./2 + 5000./4;
 
-c1 = circshift(curv1, first_shift);
-figure(60); clf; hold on;
+shift = 5000./2;
+
+c1 = circshift(curv1, first_shift1);
+figure(160); clf; hold on;
 plot (x2,c1,'b');
 
-c2 = circshift(c1, shift);
-plot (x2,c2,'r');
-
-c3 = circshift(c2, shift);
+c3 = circshift(c1, shift);
 plot (x2,c3,'g');
 
-c4 = circshift(c3, shift);
+
+c2 = circshift(curv2, first_shift2);
+plot (x2,c2,'r');
+
+c4 = circshift(c2, shift);
 plot (x2,c4,'k');
 title('Shifted sines on 5000 wide space')
 
@@ -59,12 +149,13 @@ c2d = downsample(c2,100);
 c3d = downsample(c3,100);
 c4d = downsample(c4,100);
 
-figure(70); clf; hold on;
+figure(170); clf; hold on;
 plot (x, c1d, 'bo-');
 plot (x, c2d, 'ro-');
 plot (x, c3d, 'go-');
 plot (x, c4d, 'ko-');
-title('downsampled sines on usual 50 pixel space')'
+title('downsampled sines on usual 50 pixel space');
+legend('U','L','D','R');
 
 C1 = repmat(c1d,1,50);
 USHEET = C1.*Y;
@@ -78,31 +169,42 @@ DSHEET = C3.*Y;
 C4 = repmat(c4d,1,50);
 LSHEET = C4.*Y;
 
-figure(1);
+% Set to 1 to print out pngs:
+printmaps = 0
+
+figure(101);
 surf(LSHEET);
 zlim([0 zlim_max]);
 title('Left');
-print('left.png');
+if printmaps
+    print('left.png');
+end
 
-figure(2);
+figure(102);
 surf(RSHEET);
 zlim([0 zlim_max]);
 title('Right');
-print('right.png');
+if printmaps
+    print('right.png');
+end
 
-figure(3);
+figure(103);
 surf(USHEET);
 zlim([0 zlim_max]);
 title('Up');
-print('up.png');
+if printmaps
+    print('up.png');
+end
 
-figure(4);
+figure(104);
 surf(DSHEET);
 zlim([0 zlim_max]);
 title('Down');
-print('down.png');
+if printmaps
+    print('down.png');
+end
 
-figure(6); clf;
+figure(106); clf;
 surf(USHEET.+DSHEET.+LSHEET.+RSHEET);
 zlim([0 zlim_max]);
 title('All');
