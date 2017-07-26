26390708b71b775046c73e947cd13b9dd781db33
Seb James <seb.james@sheffield.ac.uk>
2017-07-26 23:22:18 +0100

    A widening gaussian scheme which works.

--- batch_scripts/widening_gaussian/widening_gaussian.m
+++ batch_scripts/widening_gaussian/widening_gaussian.m
@@ -4,65 +4,92 @@
 
 % The neural field size - never changes in this model, so this is
 % not a parameter.
-W_nfs = 50
+W_nfs = 50;
 
 %
 % The following will be parameters to the WideningGaussian script
 %
-% The sigma constant from which sigma(r) is computed
-sigma = 25
+% The sigma multiplier from which sigma(r) is computed (as
+% sigma_m/M_f +- offsets)
+sigma_m = 40
+
+% An offset applied to sigma(r). Needs to be a new parameter.
+sigma_0 = 0.3
+
 % E2 participates in the magnification factor. In practice this is
 % not changed, but it's still included as a param.
 E2 = 2.5
-% An r shift - required in the Python script due to indexing from 0
-_rshift = 0;
+
+% A right-shift in the magnification factor. Do I want/need this? No.
+%rshift = 0;
+
 % A normalisation power
-normpower = 1.4;
+normpower = 0 %1.45
 
-r = [1+_rshift:50+_rshift];
+%r = [0+rshift:49+rshift];
+r = [1:50]; % 1-50 range here. 0-49 range of srcloc[1] in python
+            % WideningGaussian script is bumped up by 1.
 
+M_f = W_nfs ./ ( E2 .* log (( r/(2.*E2) )+1) ); % for r=0, M_f is
+                                                % infinity. Python
+                                                % can't cope.
 
-M_f =  W_nfs ./ (E2 .* log (((r)/(2.*E2))+1));
+fovshift = 20; % Past the foveal region, really, this is where the
+               % increase in the sigma starts, for r<fovshift,
+               % sigma is sigma_0
 
-rshift = 5; % Past the foveal region
-M_f(1:rshift) = M_f(rshift);
+M_f_start = W_nfs ./ ( E2 .* log (( fovshift/(2.*E2) )+1) );
+
+M_f(1:fovshift) = M_f_start;
 
 figure(1)
 plot (r, M_f);
 xlabel('r');
 ylabel('MagFactor(r)');
 title ('MagFactor');
+omsetgrid([2,2]);
 
-_sigma = sigma./M_f;
+_sigma = (sigma_m./M_f) - (sigma_m./M_f_start) + sigma_0;
+
 _SIGMA=repmat(_sigma, 50, 1);
 
 figure(2)
 plot (r, _sigma);
 xlabel('r');
-ylabel('(underscore)sigma(r)');
-title('sigma(r)');
+ylabel('\_sigma(r)');
+title('\_sigma(r)');
+omsetgrid([3,2]);
 
-d = [1:50];
-D = repmat (d, 50, 1)';
+dist = [1:50];
+DIST = repmat (dist, 50, 1)';
 
-% This gives good normalisation.
+% Power based normalisation
 normterm = 1./power(_sigma,normpower);
 
 figure(4)
 plot (r, normterm);
 title ('normterm(r)');
+omsetgrid([4,2]);
 
-
-GAUSS = normterm.*exp(-0.5.*power(D./_SIGMA,2));
+GAUSS = normterm.*exp(-0.5.*power(DIST./_SIGMA,2));
 figure(3)
 surf(GAUSS)
 xlabel('r')
 ylabel('d')
 ylim([1,15])
-view([268 0.1])
-
+%view([268 0.1])
+view([241,56])
+omsetgrid([3,1]);
 
 figure(5);
 plot (r, sum(GAUSS, 1));
 title('Sum of GAUSS surface (figure 3) along d axis')
-ylim([0 1.25])
+%ylim([0 1.25])
+omsetgrid([4,1]);
+
+hold_on = 0
+if hold_on
+    for i = [1 5]
+        figure(i); hold on;
+    end
+end
